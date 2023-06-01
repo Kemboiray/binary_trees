63045ea8ba46a3b4e747f91559bb1b3957289843
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-06-01 23:30:19 +0300

    Refactor code

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,20 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * max - return the greater of two integers
- * @a: integer
- * @b: integer
- *
- * Return: the greater of @a or @b
- */
-size_t max(size_t a, size_t b)
-{
-	if (a > b)
-		return (a);
-	return (b);
-}
-
-/**
  * binary_tree_height - Measure the height of a binary tree
  * @tree: root of binary tree
  *
@@ -29,5 +15,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
-	return (max(left_height, right_height) + 1);
+	return (left_height > right_height ? left_height + 1 : right_height + 1);
 }
